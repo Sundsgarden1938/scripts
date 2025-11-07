fe818e88799f7a3acd3aace14224fb774ec13d49
Sundsgarden1938 <filip.tvetberg@sundsgarden.se>
2025-11-07 10:06:37 +0100

    Update uninstall_teams_classic.ps1

--- uninstall_teams_classic.ps1
+++ uninstall_teams_classic.ps1
@@ -1,21 +1,34 @@
-<#
-Version: 1.0
-Author: 
-- Joey Verlinden (joeyverlinden.com)
-- Andrew Taylor (andrewstaylor.com)
-- Florian Slazmann (scloud.work)
-- Jannik Reinhard (jannikreinhard.com)
-Script: Uninstall-PrivateTeams
-Description:
-Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
-Version 1.0: Init
-Run as: Admin
-Context: 64 Bit
-#> 
+# Morganthau1
+# Teams Classic Cleanup Script
+# 3/6/2024
+# The purpose of this script is to uninstall teams classic and delete the leftover folders & cache data.
 
-try{
-    Get-AppxPackage -Name MicrosoftTeams -allusers | Remove-AppxPackage -ErrorAction stop
-    Write-Host "Private MS Teams app successfully removed"
-}catch{
-    Write-Error "Error removing Microsoft Teams app"
+# Get all user profiles from C:\Users and store them in UserProfiles variable, assign Teams Machinewide Installer location variable
+$UserProfiles = Get-ChildItem "C:\Users\" -Directory
+$InstallerPath = "C:\Program Files (x86)\Teams Installer"
+
+# Loop through each user profile
+foreach ($UserProfile in $UserProfiles) {
+    $TeamsPath = Join-Path -Path $UserProfile.FullName -ChildPath "AppData\Local\Microsoft\Teams" # Path to uninstaller location
+    $CachePath = Join-Path -Path $UserProfile.FullName -ChildPath "AppData" # Path to user AppData folder
+    
+    # Check if Teams is installed for the current user
+    if (Test-Path (Join-Path -Path $TeamsPath -ChildPath "current\Teams.exe")) {
+
+        # Stop Teams process
+        Stop-Process -Name "Teams.exe" -Force -ErrorAction SilentlyContinue
+        
+        # Uninstall Teams silently
+        Start-Process -FilePath (Join-Path -Path $TeamsPath -ChildPath "Update.exe") -ArgumentList "/uninstall /s" -Wait -PassThru
+
+        # Remove Leftover Folders
+        Remove-Item -Recurse -Force -Path $CachePath\Local\Microsoft\Teams -ErrorAction SilentlyContinue
+        Remove-Item -Recurse -Force -Path $CachePath\Local\Microsoft\TeamsPresenceAddin -ErrorAction SilentlyContinue
+        Remove-Item -Recurse -Force -Path $CachePath\Roaming\Microsoft\Teams -ErrorAction SilentlyContinue
+    }
+}
+
+# Remove Teams Installer
+if (Test-Path $InstallerPath) {
+    Remove-Item -Recurse -Force -Path $InstallerPath -ErrorAction SilentlyContinue # Since it is outside the UserProfile loop, this will run regardless of profile installations.
 }
